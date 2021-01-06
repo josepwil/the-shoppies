b08ddc41cf494d7cccc5efe3ac5850172fce97d9
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-06 02:42:54 +0000

    test: adding happy path test cases

--- test/test.js
+++ test/test.js
@@ -1,8 +1,42 @@
-const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.strictEqual([1, 2, 3].indexOf(4), -1);
-    });
+const { assert } = require('chai');
+
+describe('lookUp', function() {
+  it('should return results from OMDB if they exist', function() {
+    
+  });
+
+  it('should show list the title, year of release and contain a button to nominate that film', function() {
+
+  });
+
+  it('should update the result list when the search terms are updated', function() {
+
   });
 });
+
+describe('nomimate', function() {
+  it('should add movie to nominated list', function() {
+    
+  });
+
+  it('should not allow a nominated movie to be re-nominated', function() {
+
+  });
+
+  it('should display a banner when the user has 5 nominations', function() {
+
+  });
+
+  it('should hide the banner if the user removes a nomination', function() {
+
+  });
+});
+
+describe('removeNomination', function() {
+  it('should remove movie from nominated list', function() {
+    
+  });
+
+});
+
+
